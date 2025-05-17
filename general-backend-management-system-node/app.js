6ace3e9edb740dc4d83f586fc26bb42eabfeea59
chenwenzhuo <Chenwz1996@163.com>
2025-05-17 17:19:23 +0800

    use cors

--- general-backend-management-system-node/app.js
+++ general-backend-management-system-node/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 
-const PORT = 3007;
 const app = express();
+
+app.use(cors()); // 挂载CORS中间件
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!\n');
 });
 
+// 侦听指定端口
+const PORT = 3007;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
