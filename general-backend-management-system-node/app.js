a73a15f35424ba6083d8659d759f08deb4227249
chenwenzhuo <Chenwz1996@163.com>
2025-05-17 16:31:19 +0800

    use constant PORT

--- general-backend-management-system-node/app.js
+++ general-backend-management-system-node/app.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
+const PORT = 3007;
+
 const app = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!!!\n');
 });
 
-app.listen(3007, () => {
-  console.log('Server is running on http://localhost:3007');
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
