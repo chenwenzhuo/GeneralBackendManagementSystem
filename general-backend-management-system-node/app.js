0246f0c2dec65558a51e8826998b54551219eddc
chenwenzhuo <Chenwz1996@163.com>
2025-05-17 17:37:25 +0800

    use body-parser

--- general-backend-management-system-node/app.js
+++ general-backend-management-system-node/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(cors()); // 挂载CORS中间件
+// 挂载body-parser中间件
+app.use(bodyParser.urlencoded({extended: false})); // 解析application/x-www-form-urlencoded格式的请求体数据
+app.use(bodyParser.json()); // 解析application/json格式的请求体数据
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!\n');
