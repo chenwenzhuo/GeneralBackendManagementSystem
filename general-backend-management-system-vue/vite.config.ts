2a362192f11a79b8d291078b7d0ba845aac49573
chenwenzhuo <Chenwz1996@163.com>
2025-06-09 23:38:09 +0800

    配置 Vite 服务端参数并添加代理设置

--- general-backend-management-system-vue/vite.config.ts
+++ general-backend-management-system-vue/vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      // 配置别名 "@" 指向 "src" 文件夹
-      '@': path.resolve(__dirname, 'src'),
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            // 配置别名 "@" 指向 "src" 文件夹
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
-  },
+    server: {
+        port: 9257,
+        open: false,
+        cors: true,
+        // 配置代理
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:3007',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
 })
