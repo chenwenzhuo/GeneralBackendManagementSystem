0e4a3db43320cc825c24b7ed364fe85b57b9efb0
chenwenzhuo <Chenwz1996@163.com>
2025-06-09 23:25:51 +0800

    添加 Axios 配置文件
    - 创建 Axios 实例并配置基础参数
    - 添加请求和响应拦截器
    - 导出 Axios 实例供全局使用

---
+++ general-backend-management-system-vue/src/api/index.ts
@@ -0,0 +1,26 @@
+import axios, {AxiosInstance} from "axios";
+
+// 创建 Axios 实例
+const axiosInstance: AxiosInstance = axios.create({
+    baseURL: 'http://127.0.0.1:3007', // 基础 API 地址
+    timeout: 10000, // 10秒超时
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+});
+
+// 请求拦截器
+axiosInstance.interceptors.request.use((config) => {
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+// 响应拦截器
+axiosInstance.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export default axiosInstance;
