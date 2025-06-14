fd951d50e1f6377417022230158cab13fa22e0bf
chenwenzhuo <Chenwz1996@163.com>
2025-06-14 16:56:49 +0800

    初始化 ESLint 和 Prettier 配置
    - 添加 .eslintignore 和 .eslintrc.cjs 文件，配置 ESLint规则
    - 添加 .prettierignore 和 .prettierrc 文件，配置 Prettier 格式
    - 创建 package.json，定义项目依赖和脚本
    -配置 husky 和 lint-staged，实现 Git 前提交检查和自动格式化

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: {
+      js: 'espree',
+      ts: '@typescript-eslint/parser',
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
