cf63996f27fea76f9e7328ff6d1195d6e3c4a95a
chenwenzhuo <Chenwz1996@163.com>
2025-07-06 22:35:54 +0800

    feat(login): 实现登录页面布局并添加国际化支持
    - 添加 Login.module.scss 文件，定义登录页面样式
    - 使用 antd Layout 组件构建页面结构
    - 实现登录页面的头部、主体和底部- 添加国际化支持，使用 react-i18next
    - 引入语言切换组件 LangSwitch

--- general-backend-management-system-react/src/pages/login/Login.tsx
+++ general-backend-management-system-react/src/pages/login/Login.tsx
@@ -1,7 +1,48 @@
+import { Layout } from 'antd';
+import { useTranslation } from 'react-i18next';
+
+import LangSwitch from '@/components/lang-switch/LangSwitch.tsx';
+
+import styles from './Login.module.scss';
+
+const { Header, Footer, Content } = Layout;
+
 export default function Login() {
+  const { t } = useTranslation();
+
+  const footerText = {
+    techs: [
+      t('login.footer.devMode'),
+      t('login.footer.vue'),
+      t('login.footer.vueRouter'),
+      t('login.footer.nodejs'),
+      t('login.footer.antdVue'),
+    ],
+    copyright: [t('login.footer.copyright')],
+  };
+  const footerTechsElem = footerText.techs.map((tech, index) => <li key={index}>{tech}</li>);
+  const footerCopyrightElem = footerText.copyright.map((cr, index) => <li key={index}>{cr}</li>);
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Layout className={styles['login-layout']}>
+      <Header className={styles['login-header']}>
+        <div className={styles['header-content']}>
+          <h1 className={styles['header-title']}>{t('message.title')}</h1>
+          <div className={styles['header-shortcuts']}>
+            <div className={styles['welcome']}>{t('login.header.welcome')}</div>
+            <LangSwitch />
+          </div>
+        </div>
+      </Header>
+      <Content className={styles['login-body']}>
+        <div className={styles['body-content']}>Content</div>
+      </Content>
+      <Footer className={styles['login-footer']}>
+        <div className={styles['footer-content']}>
+          <ul className={styles['footer-techs']}>{footerTechsElem}</ul>
+          <ul className={styles['footer-copyright']}>{footerCopyrightElem}</ul>
+        </div>
+      </Footer>
+    </Layout>
   );
 }
