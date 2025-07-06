59f9beee51e6a305a1c02913480fea9f0b859b0b
chenwenzhuo <Chenwz1996@163.com>
2025-07-06 17:28:12 +0800

    build(deps): 更新依赖并添加 TypeScript 支持
    - 添加 @types/node 依赖，版本为24.0.10- 更新 globals 依赖，从16.2.0 到 16.3.0- 在 vite.config.ts 中添加路径别名和代理配置

--- general-backend-management-system-react/vite.config.ts
+++ general-backend-management-system-react/vite.config.ts
@@ -1,7 +1,27 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // 配置别名 "@" 指向 "src" 文件夹
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 9258,
+    open: false,
+    cors: true,
+    // 配置代理
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3007',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
